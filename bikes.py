85f8b53c365c53c2c300f6d5b8a708c69f2b8628
Natalia Gerasimova <natasha.desh.geras@gmail.com>
2026-01-16 14:04:20 -0500

    Updated CS files

--- bikes.py
+++ bikes.py
@@ -52,19 +52,21 @@ def main():
             Links: 45
 
     """
-    bike_wheels_provided = abs(int(float(input("How many wheels do you have? "))))
-    bike_frames_provided = abs(int(float(input("How many frames do you have? ")))) 
-    bike_links_provided = abs(int(float(input("How many links do you have? "))))
+    wheels_provided = abs(int(float(input("How many wheels do you have? "))))
+    frames_provided = abs(int(float(input("How many frames do you have? ")))) 
+    links_provided = abs(int(float(input("How many links do you have? "))))
 
-    max_bikes_from_wheels = bike_wheels_provided // BIKE_WHEELS
-    max_bikes_from_frames = bike_frames_provided // BIKE_FRAME
-    max_bikes_from_links = bike_links_provided // BIKE_LINKS
+    max_bikes_from_wheels = wheels_provided // BIKE_WHEELS
+    max_bikes_from_frames = frames_provided // BIKE_FRAME
+    max_bikes_from_links = links_provided // BIKE_LINKS
 
-    bikes_possible = min(max_bikes_from_wheels, max_bikes_from_frames, max_bikes_from_links)
+    bikes_possible = min(
+        max_bikes_from_wheels, max_bikes_from_frames, max_bikes_from_links
+    )
 
-    leftover_bike_wheels = bike_wheels_provided - (bikes_possible * BIKE_WHEELS)
-    leftover_bike_frames = bike_frames_provided - (bikes_possible * BIKE_FRAME)
-    leftover_bike_links = bike_links_provided - (bikes_possible * BIKE_LINKS)
+    leftover_bike_wheels = wheels_provided - (bikes_possible * BIKE_WHEELS)
+    leftover_bike_frames = frames_provided - (bikes_possible * BIKE_FRAME)
+    leftover_bike_links = links_provided - (bikes_possible * BIKE_LINKS)
 
     print(f"Total bikes I can make: {bikes_possible}")
     print("I'm keeping the leftovers for myself")
@@ -72,5 +74,6 @@ def main():
     print(f"{leftover_bike_wheels} wheels")
     print(f"{leftover_bike_frames} frames")
     print(f"{leftover_bike_links} links")
+
 if __name__ == "__main__":
     main()
--- shopping.py
+++ shopping.py
@@ -2,25 +2,29 @@
 Natalia Gerasimova
 CS 5001, Spring 2026
 Homework 1, Program 1 -- AmaGone.com Shopping
-
-It is Online Shopping Program
-to calculate client's shopping bag Total Cost
-and 2-Pay Plan cost. 
 '''
 INTEREST_RATE = 1.05
 PAYNMENTS_NUMBER = 2
 def main(): 
+    '''
+    This function calculates client's shopping bag Total Cost
+    and 2-Pay Plan cost based on product name, price and quantity.   
+    '''
     print("Welcome to my Online Shopping Program!")
-    input_product_name = input("Enter the product name: ")
-    input_product_price = input(f"What is the price of {input_product_name}? ")
-    input_product_quantity = input(f"How many {input_product_name}s are you purchasing? ")
-    input_product_price = abs(float(input_product_price))
-    input_product_quantity = abs(int(float(input_product_quantity)))
-    print(f"You are buying {input_product_quantity} {input_product_name} at ${input_product_price:.2f} each.")
-    total_cost = input_product_price * input_product_quantity
+    item_name = input("Enter the product name: ")
+    item_price = input(f"What is the price of {item_name}? ")
+    item_amount = input(
+        f"How many {item_name}s are you purchasing? "
+    )
+    item_price = abs(float(item_price))
+    item_amount = abs(int(float(item_amount)))
+    print(
+        f"You are buying {item_amount} {item_name} at ${item_price:.2f} each."
+    )
+    total_cost = item_price * item_amount
     print(f"Total Cost for this shopping session is ${total_cost:.2f}")
-    two_pay_plan_cost = round((total_cost * INTEREST_RATE) / PAYNMENTS_NUMBER, 2)
-    print(f"Our 2-Pay Plan is two equal payments of ${two_pay_plan_cost:.2f}")
+    two_pay_plan = round((total_cost * INTEREST_RATE) / PAYNMENTS_NUMBER, 2)
+    print(f"Our 2-Pay Plan is two equal payments of ${two_pay_plan:.2f}")
     
 if __name__ == "__main__":
     main()
